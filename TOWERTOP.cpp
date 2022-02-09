aaad4cbb50d7876d0cad7c9dca76a8b6b65945a5
gauravvsgithub <54109748+gauravvsgithub@users.noreply.github.com>
2022-02-09 22:19:36 +0530

    9feb_2

---
+++ TOWERTOP.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        ll x, m;
+        cin>>x>>m;
+        ll res = 0;
+        ll tower=0;
+        ll inventary=1;
+
+        while(m--) {
+            if((x-tower)>inventary) {
+                tower+=inventary;
+                inventary = tower;
+            }
+            else {
+                inventary -= (x-tower);
+                tower = x;
+                inventary += tower;
+                res++;
+                tower = 0;
+            }
+        }
+
+        cout<<res<<endl;
+    }
+    return 0;
+}
